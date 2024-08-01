31c22838c494ad65db2f5a41a6f21115260788cb
mano <emanuelrokah96@gmail.com>
2024-08-01 17:35:09 +0300

    adding about and info page

---
+++ src/components/Landscape/InfoComponent.vue
@@ -0,0 +1,105 @@
+<template >
+    <div class="Container">
+        <div class="textLeft">
+            <h1>
+                A Matter of  <br> <span>Taste</span>
+            </h1>
+            <p>
+                There's a reason our regulars have been coming back for more. <br> Word of mouth has been our only advertisement - but happy mouths are the best ambassadors!
+            </p>
+        </div>
+        <div class="textRight">
+            <div class="para">
+                <div class="num">1</div>
+                <div class="content">
+                    <h3>Fresh, Seasonal Ingredients</h3>
+                    <p>Fresh, Seasonal Ingredients</p>
+                </div>
+            </div>
+            <div class="para">
+                <div class="num">2</div>
+                <div class="content">
+                    <h3>Fresh, Seasonal Ingredients</h3>
+                    <p>Fresh, Seasonal Ingredients</p>
+                </div>
+            </div>
+            <div class="para">
+                <div class="num">3</div>
+                <div class="content">
+                    <h3>Fresh, Seasonal Ingredients</h3>
+                    <p>Fresh, Seasonal Ingredients</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .Container{
+        width:100vw;
+        height:100vh;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap:5vw;
+
+        font-family: 'Exo 2', sans-serif;
+    }
+    .textLeft {
+        width:30vw;
+        display: flex;
+        flex-direction: column;
+    }
+    .textLeft > h1{
+        font-size:4em;
+        color: black;
+        font-weight: 400;
+        text-align: left;
+        width:fit-content;
+        margin-bottom: 0;
+    }
+    .textLeft > h1 > span {
+        color: #FF6347; 
+    }
+    .textLeft > * {
+        overflow: hidden;
+    }
+    .textLeft > p {
+        width:70%;
+        font-size: 1.5em;
+        text-align: left;
+    }
+    .textRight {
+        background-color: #FF6347;
+        border-radius: 25px;
+        width:40vw;
+        padding-top: 5vh;
+        padding-bottom: 5vh;
+    }
+    .para{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap:3vw;
+    }
+    .para > .num{
+        font-size: 2em;
+        color:white;
+        margin-left: 5vw;
+        background-color: black;
+        border-radius: 60%;
+        width:3vw;
+        padding: 1vw;
+    }
+    .content{
+        width:100%;
+        text-align: left;
+    }
+</style>
--- src/components/Landscape/MainComponent.vue
+++ src/components/Landscape/MainComponent.vue
@@ -1,19 +1,23 @@
 <template lang="">
     <div>
-        <Header/>
+        <Header />
         <About />
+        <Info />
     </div>
 </template>
 <script>
 import Header from "@/components/Landscape/HeaderComponent.vue";
 import About from "@/components/Landscape/AboutComponent.vue"
+import Info from "@/components/Landscape/InfoComponent.vue"
 
 export default {
     components:{
         Header,
-        About
+        About,
+        Info
     }
 }
+
 </script>
 <style scoped>
     
